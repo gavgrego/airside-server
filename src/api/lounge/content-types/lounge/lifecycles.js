0728e9ef85cf97401059b24d0561f0e38a2690e3
Gavin <gavin.a.gregory@gmail.com>
2024-06-05 23:35:41 -0700

    add lifecycles to lounge for automatically adding new one to algolia

---
+++ src/api/lounge/content-types/lounge/lifecycles.js
@@ -0,0 +1,25 @@
+const getAlgoliaIndex = require("../../../../utils/algolia");
+
+const index = getAlgoliaIndex("lounge");
+
+module.exports = {
+  async afterCreate(data) {
+    const { id } = data.result;
+    // Query lounge added to database
+
+    const lounge = await strapi.entityService.findOne(
+      "api::lounge.lounge",
+      id,
+      {
+        populate: { airport: true },
+      }
+    );
+
+    try {
+      // Save the object to Algolia
+      await index.saveObject({ objectID: id, ...lounge });
+    } catch (error) {
+      console.error("Error saving object to Algolia", error);
+    }
+  },
+};
