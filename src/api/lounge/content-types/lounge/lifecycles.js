75aa2d2345040eef8dbaaad0baf1ce1afd468119
Gavin <gavin.a.gregory@gmail.com>
2024-06-05 23:41:34 -0700

    add update and delete lifecycle methods to lounge lifecycles file

--- src/api/lounge/content-types/lounge/lifecycles.js
+++ src/api/lounge/content-types/lounge/lifecycles.js
@@ -22,4 +22,33 @@ module.exports = {
       console.error("Error saving object to Algolia", error);
     }
   },
+  async afterUpdate(data) {
+    const { id } = data.result;
+    // Query lounge added to database
+
+    const lounge = await strapi.entityService.findOne(
+      "api::lounge.lounge",
+      id,
+      {
+        populate: { airport: true },
+      }
+    );
+
+    try {
+      // Save the object to Algolia
+      await index.saveObject({ objectID: id, ...lounge });
+    } catch (error) {
+      console.error("Error saving object to Algolia", error);
+    }
+  },
+  async afterDelete(data) {
+    const { id } = data.result;
+
+    try {
+      // Save the object to Algolia
+      await index.deleteObject(id);
+    } catch (error) {
+      console.error("Error saving object to Algolia", error);
+    }
+  },
 };
